48d77bd283770bd7550f6ebe8b1037bda5d1b5c1
takuya.ishida <takuya.ishida@NPC-173A.local>
2020-08-05 22:42:20 +0900

    taskbox UI

---
+++ storybook-static/precache-manifest.3a93403f2668a619a5bbeb333f78debf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b777615053c9cc61a84f8f291956e977",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d96112c89c56e16e7091.bundle.js"
+  },
+  {
+    "url": "runtime~main.d96112c89c56e16e7091.bundle.js"
+  },
+  {
+    "revision": "526596ea7e9476a6c595",
+    "url": "static/css/main.67471bcc.chunk.css"
+  },
+  {
+    "url": "vendors~main.d96112c89c56e16e7091.bundle.js"
+  },
+  {
+    "url": "vendors~main.d96112c89c56e16e7091.bundle.js.LICENSE.txt"
+  }
+]);
